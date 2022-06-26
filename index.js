fcecaa8b183ad40e040988a46fa153862c2c582e
Lenkaa3792 <allanlenkaa@gmail.com>
2022-06-26 23:42:25 +0300

    commit tree 1791aef6c8e22156c77e818d3d5b400dc5797f79

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const re = /[\W_]/g;
+
+  const lowWord = word.toLowerCase().replace(re, "");
+  const reverseWord = lowWord.split("").reverse().join("");
+  if ( reverseWord === lowWord ) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
